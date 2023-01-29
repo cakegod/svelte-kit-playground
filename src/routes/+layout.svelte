606f5bbaa99b33f30ff714eb1bb9cfb62cdb346b
Cake <cakemancerr@gmail.com>
2023-01-30 00:49:52 +0100

    feat: improve navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,23 @@
 <script>
-	import '../app.css'
+	import '../app.css';
 </script>
 
-<nav class="navbar bg-base-200">
-	<div>
+<nav class="navbar bg-neutral max-w-screen-2xl text-neutral-content rounded-box my-4 mx-auto">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<button class="btn btn-ghost">Projects</button>
+			<ul class="dropdown-content menu p-2 mt-3 shadow bg-base-200 rounded-box w-52">
+				<a class="p-4 hover:bg-base-300 transition-colors" href="/memory-game">Memory Game</a>
+				<a class="p-4 hover:bg-base-300 transition-colors" href="/shopping-cart">Shopping cart</a>
+			</ul>
+		</div>
+	</div>
+	<div class="navbar-center">
 		<a class="btn btn-ghost text-xl" href="/">Playground</a>
 	</div>
-	<a class="btn btn-ghost" href="/memory-game">Memory Game</a>
+	<div class="navbar-end" />
 </nav>
 
 <main class="max-w-7xl h-full m-auto p-4 flex flex-col items-center justify-center">
-	<slot></slot>
+	<slot />
 </main>
