1df0a7494cdd1af3551c7bacaaf2a856d774f3c5
Cake <cakemancerr@gmail.com>
2023-01-29 23:32:10 +0100

    feat: add transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,6 @@
 	<a class="btn btn-ghost" href="/memory-game">Memory Game</a>
 </nav>
 
-<main class="max-w-7xl m-auto p-4">
+<main class="max-w-7xl h-full m-auto p-4 flex flex-col items-center justify-center">
 	<slot></slot>
 </main>
--- src/routes/memory-game/+page.svelte
+++ src/routes/memory-game/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { fade, fly } from 'svelte/transition';
 	import { cardList, type Card } from './data';
 
+	let isGameReady = false;
 	let cards = cardList;
 	let bestScore = 0;
 	$: score = cards.reduce((acc, curr) => (curr.clicked ? acc + 1 : acc), 0);
@@ -25,22 +27,36 @@
 	}
 </script>
 
-<div class="stats flex">
-	<div class="stat justify-items-end">
-		<p class="stat-title">Score</p>
-		<p class="stat-value">{score}</p>
+{#if isGameReady}
+	<div class="stats flex pb-4">
+		<div class="stat justify-items-end">
+			<p class="stat-title">Score</p>
+			{#key score}
+				<p in:fly={{ y: -15, duration: 500 }} class="stat-value">{score}</p>
+			{/key}
+		</div>
+		<div class="stat">
+			<p class="stat-title">Best score</p>
+			{#key bestScore}
+				<p in:fly={{ y: -15, duration: 500 }} class="stat-value">{bestScore}</p>
+			{/key}
+		</div>
 	</div>
-	<div class="stat">
-		<p class="stat-title">Best score</p>
-		<p class="stat-value">{bestScore}</p>
-	</div>
-</div>
 
-<main class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-	{#each cards as card (card.id)}
-		<button class="btn btn-lg h-40 flex flex-col gap-2" on:click={() => handleClick(card)}>
-			<img src={card.img} class="h-14" alt="a fictional fruit from Pokémon world" />
-			<p>{card.name}</p>
-		</button>
-	{/each}
-</main>
+	<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 w-full">
+		{#each cards as card (card.id)}
+			<button
+				class="btn btn-lg h-40 flex flex-col gap-2"
+				transition:fade
+				on:click={() => handleClick(card)}
+			>
+				<img src={card.img} class="h-14" alt="a fictional fruit from Pokémon world" />
+				<p>{card.name}</p>
+			</button>
+		{/each}
+	</div>
+{:else}
+	<button class="btn btn-lg btn-wide btn-primary" on:click={() => (isGameReady = !isGameReady)}>
+		Press to start!
+	</button>
+{/if}
