4ee53c24db3a12707997c8af2f32e9185b2a2a27
Cake <cakemancerr@gmail.com>
2023-01-30 15:46:02 +0100

    feat: add initial shopping cart

---
+++ src/routes/shopping-cart/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { productsList } from './data';
+	let products = productsList;
+</script>
+
+<div transition:fade class="grid gap-10 shadow-xl sm:grid-cols-2 md:grid-cols-3">
+	{#each products as product (product.name)}
+		<div class="card bg-accent text-accent-content">
+			<figure>
+				<img class="h-full" src={product.url} alt={product.name} />
+			</figure>
+			<div class="flex flex-col gap-2 p-5">
+				<h2 class="text-base font-semibold uppercase opacity-80">{product.name}</h2>
+				<p class="text-3xl font-bold">{product.price}€</p>
+			</div>
+		</div>
+	{/each}
+</div>
---
+++ src/routes/shopping-cart/data.ts
@@ -0,0 +1,90 @@
+export type Product = {
+	name: string;
+	price: number;
+	url: string;
+	description: string;
+	quantity: number;
+};
+
+export const productsList: Product[] = [
+	{
+		name: 'Chocolate Truffle Cake',
+		price: 100,
+		url: 'https://media.bakingo.com/sq-choco-truffle-cake0005choc-A_1.jpg',
+		description:
+			'Three layers of chocolate cream filled in between stacks of chocolate cake, and adorned with chocolate icing and glaze chocolate coating.',
+		quantity: 1
+	},
+	{
+		name: 'Butterscotch Cake',
+		price: 150,
+		url: 'https://media.bakingo.com/sq-butterscotch-cake0003butt-A_1.jpg',
+		description:
+			'Three layered moist cake filled with butterscotch chips and Vanilla cream. Topped with Butterscotch glaze and white chocolate flakes, adorned with caramel dressing on the sides',
+		quantity: 1
+	},
+	{
+		name: 'Pineapple Cream Cake',
+		price: 200,
+		url: 'https://media.bakingo.com/sq-pineapple-cake0022pifr-A_2.jpg',
+		description:
+			'Three layers of pineapple cream filled in between stacks of pineapple cake, and adorned with pineapple icing and glaze pineapple coating.',
+		quantity: 1
+	},
+	{
+		name: 'Black Forest Cake',
+		price: 250,
+		url: 'https://media.bakingo.com/sq-black-forest-cake0001chbl-A_1.jpg',
+		description:
+			'Baked with the richness of cocoa and  of fresh vanilla cream, topped with soft chocolate shavings and fresh cherries.',
+		quantity: 1
+	},
+	{
+		name: 'Tropical Mango Cake',
+		price: 300,
+		url: 'https://media.bakingo.com/sq-mango-cake0020frui-A_2.jpg',
+		description:
+			'Made of three rich cream layers and mango fillings. The crest is frosted with white chocolate shavings offers mango glaze which is made from fresh mango pulp',
+		quantity: 1
+	},
+	{
+		name: 'Chocolate Marble Cake',
+		price: 350,
+		url: 'https://media.bakingo.com/chocolicious-marble-cake-cake2615choc-A.jpg',
+		description:
+			'Layered with an alluring fresh cream supported by some chocolate chips all around. It is certain that one who eats once craves for more.',
+		quantity: 1
+	},
+	{
+		name: 'Caramel Cake',
+		price: 400,
+		url: 'https://media.bakingo.com/sq-caramel-cake-cake1605cara-A_0.jpg',
+		description:
+			'Round-shaped cake covered in the soft whipped white cream topped with three chocolate swirls and heaps of cream with caramel dripping.',
+		quantity: 1
+	},
+	{
+		name: 'kitKat Chocolate Cake',
+		price: 350,
+		url: 'https://media.bakingo.com/sq-kitkat-chocolate-cake-cake1119choc-A_1.jpg',
+		description:
+			'Round-shaped KitKat cake cream filled in between stacks of chocolate cake, and adorned with chocolate icing and glaze chocolate coating.',
+		quantity: 1
+	},
+	{
+		name: 'Tiramisu Cake',
+		price: 350,
+		url: 'https://media.bakingo.com/sq-scrumptious-tiramisu-cake-cake2636exot-A.jpg',
+		description:
+			'Layered with an alluring fresh cream supported by some coffee & rum dripped ladyfingers . It is certain that one who eats once craves for more.',
+		quantity: 1
+	},
+	{
+		name: 'Assorted Fruit & Almond Cake',
+		price: 350,
+		url: 'https://media.bakingo.com/sq-fresh-fruit-cake0014frui-A_2.jpg',
+		description:
+			'Freshly baked Vanilla cake, Whipped cream and bountiful tropical fruits come together to create a treat that is even tastier than it looks.',
+		quantity: 1
+	}
+];
