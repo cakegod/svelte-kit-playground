849abc81f977fade3ac74976b722baf09a6c5a6e
Cake <cakemancerr@gmail.com>
2023-01-30 19:27:15 +0100

    feat: change projects text to icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,22 @@
 	<nav class="navbar rounded-box max-w-screen-2xl bg-accent text-accent-content xl:mx-auto">
 		<div class="navbar-start">
 			<div class="dropdown">
-				<button class="btn-ghost btn">Projects</button>
+				<button class="btn-ghost btn flex flex-wrap">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h7"
+						/>
+					</svg>
+				</button>
 				<ul class="dropdown-content menu rounded-box mt-3 w-52 bg-accent p-2 shadow">
 					<a class="p-4 transition-colors hover:bg-accent-focus" href="/memory-game">
 						Memory Game
