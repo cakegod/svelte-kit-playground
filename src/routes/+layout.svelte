fd6dedee4b5ce9f2e091384a3485a5672a5eea41
Cake <cakemancerr@gmail.com>
2023-01-30 18:34:36 +0100

    style: change colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,17 @@
 </script>
 
 <div class="p-2">
-	<nav class="navbar rounded-box max-w-screen-2xl bg-primary text-primary-content xl:mx-auto">
+	<nav class="navbar rounded-box max-w-screen-2xl bg-accent text-accent-content xl:mx-auto">
 		<div class="navbar-start">
 			<div class="dropdown">
 				<button class="btn-ghost btn">Projects</button>
-				<ul class="dropdown-content menu rounded-box mt-3 w-52 bg-primary p-2 shadow">
-					<a class="p-4 transition-colors hover:bg-primary-focus" href="/memory-game">Memory Game</a>
-					<a class="p-4 transition-colors hover:bg-primary-focus" href="/shopping-cart"
-						>Shopping cart</a
-					>
+				<ul class="dropdown-content menu rounded-box mt-3 w-52 bg-accent p-2 shadow">
+					<a class="p-4 transition-colors hover:bg-accent-focus" href="/memory-game">
+						Memory Game
+					</a>
+					<a class="p-4 transition-colors hover:bg-accent-focus" href="/shopping-cart">
+						Shopping cart
+					</a>
 				</ul>
 			</div>
 		</div>
@@ -20,7 +22,7 @@
 		</div>
 		<div class="navbar-end" />
 	</nav>
-	
+
 	<main class="m-auto flex h-full max-w-7xl flex-col items-center justify-center p-4">
 		<slot />
 	</main>
