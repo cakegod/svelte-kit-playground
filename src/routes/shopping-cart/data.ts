af1715bb3eba261cf05257026eea18d664b221ba
Cake <cakemancerr@gmail.com>
2023-01-30 23:33:50 +0100

    feat: adjust prices to be more realistic (ish)

--- src/routes/shopping-cart/data.ts
+++ src/routes/shopping-cart/data.ts
@@ -9,7 +9,7 @@ export type Product = {
 export const productsList: Product[] = [
 	{
 		name: 'Chocolate Truffle Cake',
-		price: 100,
+		price: 19.99,
 		url: 'https://media.bakingo.com/sq-choco-truffle-cake0005choc-A_1.jpg',
 		description:
 			'Three layers of chocolate cream filled in between stacks of chocolate cake, and adorned with chocolate icing and glaze chocolate coating.',
@@ -17,7 +17,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Butterscotch Cake',
-		price: 150,
+		price: 49.99,
 		url: 'https://media.bakingo.com/sq-butterscotch-cake0003butt-A_1.jpg',
 		description:
 			'Three layered moist cake filled with butterscotch chips and Vanilla cream. Topped with Butterscotch glaze and white chocolate flakes, adorned with caramel dressing on the sides',
@@ -25,7 +25,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Pineapple Cream Cake',
-		price: 200,
+		price: 99.99,
 		url: 'https://media.bakingo.com/sq-pineapple-cake0022pifr-A_2.jpg',
 		description:
 			'Three layers of pineapple cream filled in between stacks of pineapple cake, and adorned with pineapple icing and glaze pineapple coating.',
@@ -33,7 +33,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Black Forest Cake',
-		price: 250,
+		price: 49.99,
 		url: 'https://media.bakingo.com/sq-black-forest-cake0001chbl-A_1.jpg',
 		description:
 			'Baked with the richness of cocoa and  of fresh vanilla cream, topped with soft chocolate shavings and fresh cherries.',
@@ -41,7 +41,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Tropical Mango Cake',
-		price: 300,
+		price: 99.99,
 		url: 'https://media.bakingo.com/sq-mango-cake0020frui-A_2.jpg',
 		description:
 			'Made of three rich cream layers and mango fillings. The crest is frosted with white chocolate shavings offers mango glaze which is made from fresh mango pulp',
@@ -49,7 +49,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Chocolate Marble Cake',
-		price: 350,
+		price: 49.99,
 		url: 'https://media.bakingo.com/chocolicious-marble-cake-cake2615choc-A.jpg',
 		description:
 			'Layered with an alluring fresh cream supported by some chocolate chips all around. It is certain that one who eats once craves for more.',
@@ -57,7 +57,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Caramel Cake',
-		price: 400,
+		price: 99.99,
 		url: 'https://media.bakingo.com/sq-caramel-cake-cake1605cara-A_0.jpg',
 		description:
 			'Round-shaped cake covered in the soft whipped white cream topped with three chocolate swirls and heaps of cream with caramel dripping.',
@@ -65,7 +65,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'kitKat Chocolate Cake',
-		price: 350,
+		price: 49.99,
 		url: 'https://media.bakingo.com/sq-kitkat-chocolate-cake-cake1119choc-A_1.jpg',
 		description:
 			'Round-shaped KitKat cake cream filled in between stacks of chocolate cake, and adorned with chocolate icing and glaze chocolate coating.',
@@ -73,7 +73,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Tiramisu Cake',
-		price: 350,
+		price: 99.99,
 		url: 'https://media.bakingo.com/sq-scrumptious-tiramisu-cake-cake2636exot-A.jpg',
 		description:
 			'Layered with an alluring fresh cream supported by some coffee & rum dripped ladyfingers . It is certain that one who eats once craves for more.',
@@ -81,7 +81,7 @@ export const productsList: Product[] = [
 	},
 	{
 		name: 'Assorted Fruit & Almond Cake',
-		price: 350,
+		price: 49.99,
 		url: 'https://media.bakingo.com/sq-fresh-fruit-cake0014frui-A_2.jpg',
 		description:
 			'Freshly baked Vanilla cake, Whipped cream and bountiful tropical fruits come together to create a treat that is even tastier than it looks.',
