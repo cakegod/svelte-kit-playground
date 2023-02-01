1256f0f87d3a8a4addec7ed533c1dae4f2af2ccd
Cake <cakemancerr@gmail.com>
2023-02-01 21:38:48 +0100

    feat: add spinner and error

--- src/routes/weather-app/+page.svelte
+++ src/routes/weather-app/+page.svelte
@@ -3,16 +3,8 @@
 
 	let query: string;
 	let isFahrenheit = false;
-	let result = {
-		name: '',
-		country: '',
-		temp: 0,
-		description: '',
-		wind: 0,
-		humidity: 0,
-		icon: ''
-	};
-	let date = new Intl.DateTimeFormat('default', {
+	let promise = handleFetch('Marseille');
+	const date = new Intl.DateTimeFormat('default', {
 		hour: 'numeric',
 		month: 'short',
 		day: '2-digit'
@@ -28,25 +20,24 @@
 
 	async function handleFetch(query: string) {
 		const data = await getWeatherData(query);
-
-		result.name = data.name;
-		result.country = data.sys.country;
-		result.temp = Math.round(data.main.feels_like * 10) / 10;
-		result.description = data.weather[0].main;
-		result.wind = data.wind.speed;
-		result.humidity = data.main.humidity;
-		result.icon = data.weather[0].icon;
+		return {
+			name: data.name,
+			country: data.sys.country,
+			temp: Math.round(data.main.feels_like * 10) / 10,
+			description: data.weather[0].main,
+			wind: data.wind.speed,
+			humidity: data.main.humidity,
+			icon: data.weather[0].icon
+		};
 	}
 
 	function convertToF(celsius: number) {
 		return Math.round((celsius * 9) / 5 + 32 * 10) / 10;
 	}
-
-	handleFetch('Marseille');
 </script>
 
 <div class="flex w-full flex-col items-center gap-4">
-	<form on:submit={() => handleFetch(query)}>
+	<form on:submit={() => (promise = handleFetch(query))}>
 		<input
 			bind:value={query}
 			type="text"
@@ -55,80 +46,120 @@
 		/>
 	</form>
 	<p class="text-lg">{`Today, ${date}`}</p>
-	<div class="stats stats-vertical w-full max-w-sm bg-info/10 text-info-content shadow">
-		<div class="stat border-none">
-			<img
-				class="stat-figure"
-				src={`http://openweathermap.org/img/wn/${result.icon}@2x.png`}
-				alt="weather"
-			/>
-			<p class="stat-title w-20">{`${result.name}, ${result.country}`}</p>
-			<div class="stat-value flex gap-2">
-				<p>{isFahrenheit ? convertToF(result.temp) : result.temp}</p>
-				<div class="flex items-start text-xl">
-					<button
-						on:click={() => (isFahrenheit = false)}
-						class={isFahrenheit ? 'text-info-content/40' : ''}
+
+	{#await promise}
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="icon icon-tabler icon-tabler-whirl animate-spin"
+			width="50"
+			height="50"
+			viewBox="0 0 24 24"
+			stroke-width="2"
+			stroke="currentColor"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M14 12a2 2 0 1 0 -4 0a2 2 0 0 0 4 0z" />
+			<path d="M12 21c-3.314 0 -6 -2.462 -6 -5.5s2.686 -5.5 6 -5.5" />
+			<path d="M21 12c0 3.314 -2.462 6 -5.5 6s-5.5 -2.686 -5.5 -6" />
+			<path d="M12 14c3.314 0 6 -2.462 6 -5.5s-2.686 -5.5 -6 -5.5" />
+			<path d="M14 12c0 -3.314 -2.462 -6 -5.5 -6s-5.5 2.686 -5.5 6" />
+		</svg>
+	{:then result}
+		<div class="stats stats-vertical w-full max-w-sm bg-info/10 text-info-content shadow">
+			<div class="stat border-none">
+				<img
+					class="stat-figure"
+					src={`http://openweathermap.org/img/wn/${result.icon}@2x.png`}
+					alt="weather"
+				/>
+				<p class="stat-title w-20">{`${result.name}, ${result.country}`}</p>
+				<div class="stat-value flex gap-2">
+					<p>{isFahrenheit ? convertToF(result.temp) : result.temp}</p>
+					<div class="flex items-start text-xl">
+						<button
+							on:click={() => (isFahrenheit = false)}
+							class={isFahrenheit ? 'text-info-content/40' : ''}
+						>
+							{`°C`}
+						</button>
+						<span class="font-normal">|</span>
+						<button
+							on:click={() => (isFahrenheit = true)}
+							class={!isFahrenheit ? 'text-info-content/40' : ''}
+						>
+							{`°F`}
+						</button>
+					</div>
+				</div>
+				<p class="stat-title">{result.description}</p>
+			</div>
+			<div class="stat border-none">
+				<div class="stat-title flex gap-1">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-wind"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
 					>
-						{`°C`}
-					</button>
-					<span class="font-normal">|</span>
-					<button
-						on:click={() => (isFahrenheit = true)}
-						class={!isFahrenheit ? 'text-info-content/40' : ''}
-					>
-						{`°F`}
-					</button>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M5 8h8.5a2.5 2.5 0 1 0 -2.34 -3.24" />
+						<path d="M3 12h15.5a2.5 2.5 0 1 1 -2.34 3.24" />
+						<path d="M4 16h5.5a2.5 2.5 0 1 1 -2.34 3.24" />
+					</svg>
+					<p>Wind Speed</p>
+				</div>
+				<div class="stat-value flex items-center gap-2">
+					<p>{`${result.wind.toFixed(1)} km/h`}</p>
 				</div>
 			</div>
-			<p class="stat-title">{result.description}</p>
+			<div class="stat border-none">
+				<div class="stat-title flex gap-1">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-droplet"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M6.8 11a6 6 0 1 0 10.396 0l-5.197 -8l-5.2 8z" />
+					</svg>
+					<p>Humidity</p>
+				</div>
+				<p class="stat-value">{`${result.humidity}%`}</p>
+			</div>
 		</div>
-		<div class="stat border-none">
-			<div class="stat-title flex gap-1">
+	{:catch}
+		<div class="alert alert-error shadow-lg">
+			<div>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					class="icon icon-tabler icon-tabler-wind"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					stroke-width="2"
-					stroke="currentColor"
+					class="h-6 w-6 flex-shrink-0 stroke-current"
 					fill="none"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<path d="M5 8h8.5a2.5 2.5 0 1 0 -2.34 -3.24" />
-					<path d="M3 12h15.5a2.5 2.5 0 1 1 -2.34 3.24" />
-					<path d="M4 16h5.5a2.5 2.5 0 1 1 -2.34 3.24" />
+					viewBox="0 0 24 24"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/>
 				</svg>
-				<p>Wind Speed</p>
-			</div>
-			<div class="stat-value flex items-center gap-2">
-				<p>{`${result.wind.toFixed(1)} km/h`}</p>
+				<span>Error! Check the city name and try again.</span>
 			</div>
 		</div>
-		<div class="stat border-none">
-			<div class="stat-title flex gap-1">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="icon icon-tabler icon-tabler-droplet"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					stroke-width="2"
-					stroke="currentColor"
-					fill="none"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<path d="M6.8 11a6 6 0 1 0 10.396 0l-5.197 -8l-5.2 8z" />
-				</svg>
-				<p>Humidity</p>
-			</div>
-			<p class="stat-value">{`${result.humidity}%`}</p>
-		</div>
-	</div>
-	<div class="stats bg-primary text-primary-content" />
+	{/await}
 </div>
