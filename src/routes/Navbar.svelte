8a3623ebb5800e3f2e6e9efd36a4d8a21b061245
Cake <cakemancerr@gmail.com>
2023-02-04 17:22:19 +0100

    feat: dynamic project links!

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,11 +1,26 @@
+<script context="module" lang="ts">
+	const modules = import.meta.glob('./**/+page.svelte');
+
+	interface ProjectLink {
+		route: string;
+		name: string;
+	}
+
+	const projectLinks = Object.keys(modules).reduce((acc, curr) => {
+		const clean = curr.replace('+page.svelte', '').slice(1).split('/')[1];
+		const projectPath = '/' + clean;
+		const projectName = clean
+			.split('-')
+			.map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+			.join(' ');
+		return acc.some((item) => item.name === projectName) || projectName === ''
+			? acc
+			: [...acc, { route: projectPath, name: projectName }];
+	}, [] as ProjectLink[]);
+</script>
+
 <script lang="ts">
 	import { IconAlignJustified } from '@tabler/icons-svelte';
-
-	const links = [
-		{ route: 'memory-game', name: 'Memory Game' },
-		{ route: 'shopping-cart', name: 'Shopping Cart' },
-		{ route: 'weather-app', name: 'Weather App' }
-	];
 </script>
 
 <nav class="navbar rounded-box max-w-screen-2xl bg-accent text-accent-content xl:mx-auto">
@@ -14,8 +29,8 @@
 			<IconAlignJustified />
 		</button>
 		<ul class="dropdown-content menu rounded-box mt-3 w-52 bg-accent p-2 shadow">
-			{#each links as link}
-				<a class="p-4 transition-colors hover:bg-accent-focus" href={`/${link.route}`}>
+			{#each projectLinks as link}
+				<a class="p-4 transition-colors hover:bg-accent-focus" href={`${link.route}`}>
 					{link.name}
 				</a>
 			{/each}
