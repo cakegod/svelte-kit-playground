f32faa5f40a27369efb58fa06bf5c22f53b870ba
Cake <cakemancerr@gmail.com>
2023-02-04 16:19:42 +0100

    feat: initial todo project

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	interface Task {
+		name: string;
+		description: string;
+		done: boolean;
+	}
+
+	interface Project {
+		name: string;
+		tasks: string[];
+	}
+
+	interface Todo {
+		projects: { [key: string]: Project };
+		tasks: { [key: string]: Task };
+	}
+
+	const firstTaskId = crypto.randomUUID();
+	const secondTaskId = crypto.randomUUID();
+
+	const todo: Todo = {
+		projects: {
+			[crypto.randomUUID()]: {
+				name: 'Default project',
+				tasks: [firstTaskId, secondTaskId]
+			}
+		},
+		tasks: {
+			[firstTaskId]: { name: 'First Task', description: 'some description...', done: false },
+			[secondTaskId]: { name: 'Second Task', description: 'some description...', done: false }
+		}
+	};
+</script>
