5133bada05a1e448377d589211aba6013c90bf81
Cake <cakemancerr@gmail.com>
2023-02-04 16:19:27 +0100

    style: improve total cart

--- src/routes/shopping-cart/cart/+page.svelte
+++ src/routes/shopping-cart/cart/+page.svelte
@@ -11,34 +11,44 @@
 
 <div class="flex w-full flex-col gap-4">
 	{#each $cart as product}
-		<div
-			class="card card-side card-compact w-full max-w-md gap-2 bg-secondary text-secondary-content"
-		>
+		<div class="card card-side card-compact w-full max-w-md gap-2 bg-base-300 text-base-content">
 			<figure class="w-32">
 				<img class="w-full" src={product.url} alt={product.name} />
 			</figure>
 			<div class="flex flex-1 flex-col justify-evenly text-base font-bold  ">
-				<h2 class="text-secondary-content/75">{product.name}</h2>
+				<h2 class="text-base-content/75">{product.name}</h2>
 				<p class="text-lg">{product.price}€</p>
-				<div class="flex items-center gap-2">
-					<span
+				<div class="input-group flex items-center">
+					<button
 						on:click={() => cart.decrementQuantity(product)}
 						on:keydown={() => cart.decrementQuantity(product)}
-						class="btn-sm btn flex items-center justify-center "
+						class="btn-secondary btn-xs btn flex items-center justify-center "
 					>
 						-
-					</span>
-					{product.quantity}
-					<span
+					</button>
+					<input
+						type=""
+						value={product.quantity}
+						class="input-secondary input input-xs flex w-12"
+					/>
+					<button
 						on:click={() => cart.incrementQuantity(product)}
 						on:keydown={() => cart.incrementQuantity(product)}
-						class="btn-sm btn flex items-center justify-center "
+						class="btn-secondary btn-xs btn flex items-center justify-center "
 					>
 						+
-					</span>
+					</button>
 				</div>
 			</div>
 		</div>
 	{/each}
-	<p>Total: {total}</p>
+	<div class="stat rounded-box text-primary-content">
+		<div>
+			<div class="stat-title">Total</div>
+			<div class="stat-value">{total}</div>
+		</div>
+		<div class="stat-actions">
+			<button class="btn-primary btn">Checkout</button>
+		</div>
+	</div>
 </div>
