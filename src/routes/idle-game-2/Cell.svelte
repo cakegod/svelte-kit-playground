d63522d39d87ada790004806faf452ff02ab10a2
Cake <cakemancerr@gmail.com>
2023-02-06 17:35:05 +0100

    feat: extract cell

---
+++ src/routes/idle-game-2/Cell.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { currency } from './store';
+
+	export let color: string;
+</script>
+
+<div class={`h-3 w-3 border border-gray-900 ${color}`} />
--- src/routes/idle-game-2/Cells.svelte
+++ src/routes/idle-game-2/Cells.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Cell from './Cell.svelte';
+
 	import { currency } from './store';
 
 	export let filledColor: string, emptyColor: string, color: keyof typeof $currency;
@@ -6,10 +8,6 @@
 
 <div class="grid grid-cols-10 border border-gray-900">
 	{#each Array(10) as _, index}
-		<div
-			class={`h-3 w-3 border border-gray-900 ${
-				index < Number($currency[color]) ? filledColor : emptyColor
-			}`}
-		/>
+		<Cell color={index < Number($currency[color]) ? filledColor : emptyColor} />
 	{/each}
 </div>
