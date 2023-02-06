d45c7b562ec36c6a99805fccd34762234421a1c9
Cake <cakemancerr@gmail.com>
2023-02-06 18:45:58 +0100

    chore: extract color names

--- src/routes/idle-game-2/data.ts
+++ src/routes/idle-game-2/data.ts
@@ -12,4 +12,5 @@ export const BLUE = { color: 'blue', filled: 'bg-blue-500', empty: 'bg-blue-900'
 export const INDIGO = { color: 'indigo', filled: 'bg-indigo-500', empty: 'bg-indigo-900' } as const;
 export const VIOLET = { color: 'violet', filled: 'bg-violet-500', empty: 'bg-violet-900' } as const;
 
-export const CELL_COLORS = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET] satisfies Color[];
+export const CURRENCY_COLORS = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET] satisfies Color[];
+export const COLORS_NAME = CURRENCY_COLORS.map((currency) => currency.color);
