4b6f7bd875f1fb49fe67467de7ab4b0bf4e1c99f
Cake <cakemancerr@gmail.com>
2023-02-06 21:11:54 +0100

    fix: tooltip

--- src/routes/idle-game-2/Tooltip.svelte
+++ src/routes/idle-game-2/Tooltip.svelte
@@ -1,35 +1,29 @@
-<script>
-	export let title = '';
+<script lang="ts">
 	let isHovered = false;
-	let x;
-	let y;
-	
-	function mouseOver(event) {
+	let x: number;
+	let y: number;
+
+	function mouseOver(event: MouseEvent) {
 		isHovered = true;
 		x = event.pageX + 5;
 		y = event.pageY + 5;
 	}
-	function mouseMove(event) {
+	function mouseMove(event: MouseEvent) {
 		x = event.pageX + 5;
 		y = event.pageY + 5;
 	}
 	function mouseLeave() {
 		isHovered = false;
 	}
-
-
 </script>
 
-<div
-	on:mouseover={mouseOver}
-  on:mouseleave={mouseLeave}
-	on:mousemove={mouseMove}>
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div on:mouseover={mouseOver} on:mouseleave={mouseLeave} on:mousemove={mouseMove}>
 	<slot />
 </div>
 
 {#if isHovered}
 	<div style="top: {y}px; left: {x}px;" class="">
-		<slot {test} name="tooltip-content" />
-
+		<slot name="tooltip-content" />
 	</div>
 {/if}
