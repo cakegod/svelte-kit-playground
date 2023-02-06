a4bf8207f21a67d920a538d410a9850e69551ff1
Cake <cakemancerr@gmail.com>
2023-02-06 22:19:19 +0100

    chore: extract upgrades to a component

--- src/routes/idle-game-2/+page.svelte
+++ src/routes/idle-game-2/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import { IconMouse2 } from '@tabler/icons-svelte';
-	import Cell from './Cell.svelte';
-	import Cells from './Cells.svelte';
-	import Tooltip from './Tooltip.svelte';
-	import { COLORS_NAME, CURRENCY_COLORS } from './data';
-	import { currency, Upgrade, upgrades } from './store';
-	import { slide } from 'svelte/transition';
-	import { onMount } from 'svelte';
+	import Upgrades from './Upgrades.svelte';
 
-	let rawCurrency = 0;
+	import Cells from './Cells.svelte';
+	import { currency, rawCurrency, upgrades } from './store';
+	import { onMount } from 'svelte';
+	import { CURRENCY_COLORS } from './data';
 
 	let lastTime: number;
 	let totalTime = 0;
@@ -33,38 +29,14 @@
 	function updateGame(deltaTime: number, totalTime: number) {
 		// rawCurrency += (deltaTime / 1000) * 1;
 
-		rawCurrency += $upgrades.reduce((acc, curr) => {
-			return (acc += (curr.amount * curr.power * deltaTime) / curr.interval);
-		}, 0);
-	}
-	$: currency.updateCurrency(Math.round(rawCurrency));
-
-	function buyUpgrade(upgrade: Upgrade) {
-		if (rawCurrency < upgrade.getPrice()) return;
-		rawCurrency -= upgrade.getPrice();
-
-		upgrades.update(
-			$upgrades.map((u) => {
-				if (u.name === upgrade.name) {
-					upgrade.amount += 1;
-					upgrade.amount % 3 === 0 && (upgrade.power += 1);
-
-					return upgrade;
-				}
-				return u;
-			})
+		rawCurrency.increase(
+			$upgrades.reduce(
+				(acc, curr) => (acc += (curr.amount * curr.power * deltaTime) / curr.interval),
+				0
+			)
 		);
 	}
-
-	function rawCurrencyToCurrencyColor(price: number, name?: boolean) {
-		const stringifiedPrice = String(price).split('');
-		if (name) {
-			return CURRENCY_COLORS.find(
-				(type) => type.color === COLORS_NAME[stringifiedPrice.length - 1]
-			)!.filled;
-		}
-		return `${stringifiedPrice[0]}${stringifiedPrice[0 + 1] ? '.' + stringifiedPrice[0 + 1] : ''}`;
-	}
+	$: currency.updateCurrency(Math.round($rawCurrency));
 </script>
 
 <div class="flex gap-4">
@@ -74,44 +46,9 @@
 </div>
 
 <!-- {#key rawCurrency} -->
-<button class="btn-error btn-lg btn m-4" on:click={() => (rawCurrency += 1)}>Click</button>
+<button class="btn-error btn-lg btn m-4" on:click={() => rawCurrency.increase(1)}
+	>Click</button
+>
 <!-- {/key} -->
 
-{#each $upgrades as upgrade}
-	<div class="flex flex-col items-center gap-1">
-		<div class="flex w-full justify-between">
-			<span class="badge-accent badge gap-1 font-bold">
-				{rawCurrencyToCurrencyColor(upgrade.getPrice())}
-				<Cell color={rawCurrencyToCurrencyColor(upgrade.getPrice(), true)} /></span
-			>
-			<span class="badge-accent badge font-bold">{upgrade.amount}</span>
-		</div>
-		<Tooltip>
-			<button
-				disabled={upgrade.getPrice() > rawCurrency}
-				on:click={() => buyUpgrade(upgrade)}
-				class="rounded-box btn flex h-20 w-20 flex-col gap-2"
-			>
-				<IconMouse2 size={34} />
-			</button>
-
-			<div
-				transition:slide
-				slot="tooltip-content"
-				class="rounded-box absolute flex w-52 -translate-x-1/2 flex-col gap-1 bg-primary/25 p-4 text-primary-content shadow"
-			>
-				<p>Creates a cursor that automatically presses the button every 2 seconds</p>
-				<div>
-					<p class="font-bold text-primary-content/75">
-						{`Cursors: ${upgrade.amount} -> ${upgrade.amount + 1}`}
-					</p>
-					<p class="font-bold text-primary-content/75">
-						{`Click power: ${upgrade.power} -> ${
-							upgrade.amount % 3 === 0 ? upgrade.power + 1 : upgrade.power
-						}`}
-					</p>
-				</div>
-			</div>
-		</Tooltip>
-	</div>
-{/each}
+<Upgrades />
---
+++ src/routes/idle-game-2/Upgrades.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { IconMouse2 } from '@tabler/icons-svelte';
+	import Cell from './Cell.svelte';
+	import Tooltip from './Tooltip.svelte';
+	import { COLORS_NAME, CURRENCY_COLORS } from './data';
+	import { rawCurrency, Upgrade, upgrades } from './store';
+	import { slide } from 'svelte/transition';
+
+	function rawCurrencyToCurrencyColor(price: number, name?: boolean) {
+		const stringifiedPrice = String(price).split('');
+		if (name) {
+			return CURRENCY_COLORS.find(
+				(type) => type.color === COLORS_NAME[stringifiedPrice.length - 1]
+			)!.filled;
+		}
+		return `${stringifiedPrice[0]}${stringifiedPrice[0 + 1] ? '.' + stringifiedPrice[0 + 1] : ''}`;
+	}
+
+	function buyUpgrade(upgrade: Upgrade) {
+		if ($rawCurrency < upgrade.getPrice()) return;
+		rawCurrency.decrease(upgrade.getPrice());
+
+		upgrades.update(
+			$upgrades.map((u) => {
+				if (u.name === upgrade.name) {
+					upgrade.amount += 1;
+					upgrade.amount % 3 === 0 && (upgrade.power += 1);
+
+					return upgrade;
+				}
+				return u;
+			})
+		);
+	}
+</script>
+
+{#each $upgrades as upgrade}
+	<div class="flex flex-col items-center gap-1">
+		<div class="flex w-full justify-between">
+			<span class="badge-accent badge gap-1 font-bold">
+				{rawCurrencyToCurrencyColor(upgrade.getPrice())}
+				<Cell color={rawCurrencyToCurrencyColor(upgrade.getPrice(), true)} /></span
+			>
+			<span class="badge-accent badge font-bold">{upgrade.amount}</span>
+		</div>
+		<Tooltip>
+			<button
+				disabled={upgrade.getPrice() > $rawCurrency}
+				on:click={() => buyUpgrade(upgrade)}
+				class="rounded-box btn flex h-20 w-20 flex-col gap-2"
+			>
+				<IconMouse2 size={34} />
+			</button>
+
+			<div
+				transition:slide
+				slot="tooltip-content"
+				class="rounded-box absolute flex w-52 -translate-x-1/2 flex-col gap-1 bg-primary/25 p-4 text-primary-content shadow"
+			>
+				<p>Creates a cursor that automatically presses the button every 2 seconds</p>
+				<div>
+					<p class="font-bold text-primary-content/75">
+						{`Cursors: ${upgrade.amount} -> ${upgrade.amount + 1}`}
+					</p>
+					<p class="font-bold text-primary-content/75">
+						{`Click power: ${upgrade.power} -> ${
+							upgrade.amount % 3 === 0 ? upgrade.power + 1 : upgrade.power
+						}`}
+					</p>
+				</div>
+			</div>
+		</Tooltip>
+	</div>
+{/each}
