135181c1b91fbefd8d5e31ab61c697940c5d8ff8
Cake <cakemancerr@gmail.com>
2023-02-07 00:22:56 +0100

    feat: add interval to mimic cursors

--- src/routes/idle-game-2/classes.ts
+++ src/routes/idle-game-2/classes.ts
@@ -1,3 +1,6 @@
+import { get } from 'svelte/store';
+import { experience, rawCurrency, upgrades } from './store';
+
 export class Upgrade {
 	level: number;
 	name: string;
@@ -25,7 +28,57 @@ export class Upgrade {
 	getPrice() {
 		return Math.round(this.price * this.priceMultiplier ** this.level);
 	}
-}
 
-const CURSOR = new Upgrade('cursor', 5, 1, 2.25, 2000);
-export const UPGRADES_LIST = [CURSOR];
+	// TODO: extract this to cursor upgrade ONLY
+	buyUpgrade() {
+		if (get(rawCurrency) < this.getPrice()) return;
+		rawCurrency.decrease(this.getPrice());
+
+		// const timeoutList: NodeJS.Timeout[] = [];
+		// const click = () => {
+		// 	const timeout = setTimeout(() => {
+		// 		rawCurrency.increase(this.power);
+		// 		experience.increaseCursor(this.power);
+		// 		timeoutList.push(timeout);
+		// 		console.log(timeout);
+
+		// 		click();
+		// 	}, this.interval);
+		// };
+
+		// click();
+
+		const id = setInterval(() => {
+			rawCurrency.increase(this.power);
+			experience.increaseCursor(this.power);
+		}, this.interval);
+		ids.push(id);
+
+		upgrades.update(
+			get(upgrades).map((u) => {
+				if (u.name === this.name) {
+					if (this.level > 0 && this.level % 4 === 0) {
+						this.amount -= 3;
+
+						// TODO: refactor interval removal
+						// experimental fix
+						ids.forEach((item, i) => {
+							i !== 3 && clearInterval(item);
+						});
+						this.power += 100;
+					} else {
+						this.amount += 1;
+					}
+					this.level += 1;
+					return this;
+				}
+				return u;
+			})
+		);
+	}
+}
+const ids: NodeJS.Timer[] = [];
+export const UPGRADES_LIST = [
+	new Upgrade('cursor', 1, 1, 1.1, 2000),
+	new Upgrade('something else', 50, 100, 2.25, 2000)
+];
